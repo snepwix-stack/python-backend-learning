b5b5c6e07d11e0436b2a00fe89bd612b4b6f4d95
snepwix <snepwix@gmail.com>
2026-03-08 23:19:55 +0500

    Change output language in describe_car function

--- args_kwargs.py
+++ args_kwargs.py
@@ -1,6 +1,7 @@
 def describe_car(brand, model, **features):
-    print(f"Автомобиль: {brand} {model}")
+    print(f"Car: {brand} {model}")
     for key, value in features.items():
         print(f"{key}: {value}")
+
 
 describe_car("Tesla", "Model 3", color="red", autopilot=True)
