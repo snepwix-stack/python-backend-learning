cf172e5f735233a7200b51f8fab2d348474ae43d
snepwix <snepwix@gmail.com>
2026-04-03 18:41:07 +0500

    feat: v3 service layer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from storage import load_tasks, save_tasks
-from models import Task
+from services import add_task, delete_task, delete_all_tasks, show_tasks
 
 
 def show_menu():
@@ -8,53 +7,6 @@ def show_menu():
     )
     choice = input("Выберите операцию: ")
     return choice
-
-
-def add_task():
-    t = input("Ваша задача: ")
-    p = input("Приоритет задачи: ")
-    tasks = load_tasks()
-    task = Task(id=len(tasks) + 1, text=t, priority=p)
-    tasks.append(task)
-    save_tasks(tasks)
-
-
-def show_tasks():
-    tasks = load_tasks()
-    if not tasks:
-        print("Список задач пуст.")
-        return
-    for v in tasks:
-        print(v)
-
-
-def delete_task():
-    show_tasks()
-    tasks = load_tasks()
-    if not tasks:
-        return
-    try:
-        id = int(input("Введите ID задачи чтобы ее удалить: "))
-        count = len(tasks)
-        tasks = [task for task in tasks if task.id != id]
-
-        if len(tasks) == count:
-            print(f"Задача с ID {id} не найдена.")
-        else:
-            save_tasks(tasks)
-            print(f"Задача №{id} удалена.")
-
-    except ValueError:
-        print("ОШИБКА! Вы ввели неверный ID.")
-
-
-def delete_all_tasks():
-    q = input("Вы уверены? y/n: ")
-    if q == "n":
-        return
-    else:
-        tasks = []
-        save_tasks(tasks)
 
 
 while True:
